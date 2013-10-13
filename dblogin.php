7f26c47625939b4b3e605dd45d629c2f700912d2
AdrianaG77 <kermit_and_piggy@yahoo.com>
2013-10-13 13:21:08 -0500

    Convert from mysql to mysqli

--- dblogin.php
+++ dblogin.php
@@ -4,10 +4,10 @@ $db_database = 'sampledb';
 $db_username = 'sampleun';
 $db_password = 'sample pw';
 
-$db_server = mysql_connect($db_hostname, $db_username, $db_password);
+$db_server = mysqli_connect($db_hostname,$db_username,$db_password,$db_database)
 
-if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
-            mysql_select_db($db_database) or die("Unable to select database: " . mysql_error());
+if (!$db_server) die("Unable to connect to MySQL: " . mysqli_error($db_server);
+           
             
             
 /* Stop injections */
