0e3bd77e7dcc39bba48f044e6ac972b2869d8e5e
Andrew Spalding <Andrew@Zachary-Ariess-MacBook-Pro-2.local>
2013-10-27 20:15:44 -0500

    final db. cleanup.

--- db.sql
+++ db.sql
@@ -18,6 +18,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `spProject`
 --
+
+-- DROP DATABASE `sp361_project`;
 CREATE DATABASE `sp361_project` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `sp361_project`;
 
@@ -28,33 +30,32 @@ USE `sp361_project`;
 --
 
 CREATE TABLE IF NOT EXISTS `rooms` (
-  `room_ID` int(3) NOT NULL DEFAULT '',
+  `room_ID` int(3) NOT NULL,
   `room_name` varchar(100) DEFAULT NULL,
   `type` varchar(100) DEFAULT NULL,
   `non/smoking` tinyint(1) UNSIGNED NOT NULL,
   `pet` tinyint(1) UNSIGNED NOT NULL,
   `vacancy` tinyint(1) UNSIGNED NOT NULL,
-  `meal` varchar(25) DEFAULT NULL,
+  `meal` varchar(100) DEFAULT NULL,
   `rating` char(5) DEFAULT NULL,
   `price` char(5) DEFAULT NULL,
-  `guest name` longtext,
   PRIMARY KEY (`room_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `rooms`
 --
 
-INSERT INTO `rooms` (`room_ID`, `room_name`, `type`, `non/smoking`, `pet`, `vacancy`, `meal`, `rating`, `price`, `guest name`) VALUES
-(900, 'Candy Castle', 'Penthouse', 1, 1, 0, 'All Inclusive + Bar', '*****', '$$$$$', 'testGuest'),
-(800, 'Gingerbread Plum Trees', 'Centennial Suite', 0, 0, 0, 'All Inclusive', '*****', '$$$$', 'testGuest'),
-(700, 'Gumdrop Mountains', 'Centennial Suite', 1, 1, 0, 'All Inclusive', '*****', '$$$$', 'testGuest'),
-(600, 'Licorice Castle', 'Executive Suite', 0, 0, 0, 'Breakfast and Lunch or Dinner', '****', '$$$$', 'testGuest'),
-(500, 'Lollipop Woods', 'Executive Suite', 1, 1, 0, 'Breakfast and lunch or dinner', '****', '$$$$', 'testGuest'),
-(400, 'Molasses Swamp', 'Deluxe Room 1 King/2 full', 0, 0, 0, 'Breakfast', '***', '$$$', 'testGuest'),
-(300, 'Peanut Brittle House', 'Deluxe Room 1 King/2 full', 1, 1, 0, 'Breakfast', '***', '$$$', 'testGuest'),
-(200, 'Peppermint Forest', 'Standard Room 1 King/2 full', 0, 0, 0, 'none', '***', '$$', 'testGuest');
-(100, 'Ice Cream Sea', 'Standard Room 1 King/2 full', 1, 1, 0, 'none', '***', '$$', 'testGuest');
+INSERT INTO `rooms` (`room_ID`, `room_name`, `type`, `non/smoking`, `pet`, `vacancy`, `meal`, `rating`, `price`) VALUES
+(900, 'Candy Castle', 'Penthouse', 1, 1, 0, 'All Inclusive + Bar', '*****', '$$$$$'),
+(800, 'Gingerbread Plum Trees', 'Centennial Suite', 0, 0, 0, 'All Inclusive', '*****', '$$$$'),
+(700, 'Gumdrop Mountains', 'Centennial Suite', 1, 1, 0, 'All Inclusive', '*****', '$$$$'),
+(600, 'Licorice Castle', 'Executive Suite', 0, 0, 0, 'Breakfast and Lunch or Dinner', '****', '$$$$'),
+(500, 'Lollipop Woods', 'Executive Suite', 1, 1, 0, 'Breakfast and lunch or dinner', '****', '$$$$'),
+(400, 'Molasses Swamp', 'Deluxe Room 1 King/2 full', 0, 0, 0, 'Breakfast', '***', '$$$'),
+(300, 'Peanut Brittle House', 'Deluxe Room 1 King/2 full', 1, 1, 0, 'Breakfast', '***', '$$$'),
+(200, 'Peppermint Forest', 'Standard Room 1 King/2 full', 0, 0, 0, 'none', '***', '$$'),
+(100, 'Ice Cream Sea', 'Standard Room 1 King/2 full', 1, 1, 0, 'none', '***', '$$');
 
 -- --------------------------------------------------------
 
@@ -64,17 +65,50 @@ INSERT INTO `rooms` (`room_ID`, `room_name`, `type`, `non/smoking`, `pet`, `vaca
 
 CREATE TABLE IF NOT EXISTS `users` (
   `name` varchar(128) DEFAULT NULL,
-  `type` enum('underclass','upperclass','alumni','faculty') DEFAULT NULL,
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` enum('admin','user') DEFAULT NULL,
+  `user_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `pass` varchar(32) DEFAULT NULL,
-  `courses` longtext,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  PRIMARY KEY (`user_ID`)
+) ENGINE=InnoDB   DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`name`, `type`, `id`, `pass`, `courses`) VALUES
-('test', 'faculty', 1, '098f6bcd4621d373cade4e832627b4f6', NULL),
-('testStudent', 'underclass', 3, '098f6bcd4621d373cade4e832627b4f6', NULL);
+INSERT INTO `users` (`name`, `type`, `user_ID`, `pass`) VALUES
+('Abi Normal', 'admin', 1, '098f6bcd4621d373cade4e832627b4f6');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `history`
+--
+
+CREATE TABLE `history`(
+`h_room_ID` int(3) NOT NULL,
+`h_user_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (`h_room_ID`, `h_user_ID`),
+CONSTRAINT history_h_room_ID 
+FOREIGN KEY (`h_room_ID`) REFERENCES rooms (`room_ID`) ON DELETE CASCADE,
+CONSTRAINT history_h_user_ID 
+FOREIGN KEY (`h_user_ID`) REFERENCES users (`user_ID`) ON DELETE CASCADE
+)
+ENGINE=INNODB;
+
+INSERT INTO `history` (`h_room_ID`, `h_user_ID`) VALUES
+(900, 1);
+INSERT INTO `history` (`h_room_ID`, `h_user_ID`) VALUES
+(700, 1);
+
+DELETE FROM rooms where room_ID = 900;
+DELETE FROM users where user_ID = 1;
+
+
+SELECT rooms.room_ID, users.name
+FROM rooms, users, history 
+WHERE (users.user_ID = history.h_user_ID and rooms.room_ID = history.h_room_ID and users.user_ID = 1)
+
+
+
+
+
--- index.php
+++ index.php
@@ -56,40 +56,6 @@ require_once 'dblogin.php';
 			echo $twig->render('@user/view_table.html', array('rows' => $rows));
 		}
 
-		
-		/*
-        echo '<table class="table table-bordered"><tr>';
-        echo '<th></th><th>Hotel</th><th>Date</th><th>Location</th> <th>Vacancy</th> <th>Filled</th> <th>Waitlist</th><th>Rating</th><th>Notes</th><th>City</th>';
-        echo '</tr>';
-        
-        if($type == "faculty")        
-            echo '<form name="input" action="edit_suites.php" method="get">';
-        else
-            echo '<form name="input" action="suites_registered.php" method="get">';
-
-        for ($j = 0 ; $j < $rows ; ++$j)
-        {
-            $row = mysql_fetch_row($result);
-            echo "<tr><td><input type='radio' name='Register' value='$row[6]'>";
-            for ($k = 0 ; $k < 8 ; ++$k) echo "<td>$row[$k]</td>";
-                echo "</tr>";
-        }
-        if (!isset($_SESSION['name'])){
-            echo 'You must register to book a suite.</br></br>';
-        }
-
-        else if($type == "faculty"){
-            echo '</table><input type="submit" value="Edit"></form>';
-            echo '<form name="input" action="edit_suites.php" method="get">';
-            echo "<input type='hidden' id='type' name='Register' value='new'>";
-            echo '<input type="submit" value="New Course">';
-            echo '</form>';
-        }
-        else if ($type == "underclass"){
-            echo '</table><input type="submit" value="Register"></form>';
-        }
-  		
-	*/
 	?>
 	
 </div>
