e6e5bea7ca8f624e5fdbf3b614934ffb890fcf3b
Andrew <andrewtspalding@gmail.com>
2013-11-05 13:36:28 -0600

    removed incomplete work

--- dblogin.php
+++ dblogin.php
@@ -1,13 +1,13 @@
 <?php
-$db_hostname = 'localhost';
-$db_database = 'sampledb';
-$db_username = 'sampleun';
-$db_password = 'sample pw';
+$db_hostname = 'sample host';
+$db_database = 'sample db';
+$db_username = 'sample user';
+$db_password = 'sample password';
 
-$db_server = mysqli_connect($db_hostname,$db_username,$db_password,$db_database)
+$db_server = mysql_connect($db_hostname, $db_username, $db_password);
 
-if (!$db_server) die("Unable to connect to MySQL: " . mysqli_error($db_server);
-           
+if (!$db_server) die("Unable to connect to MySQL: " . mysql_error());
+            mysql_select_db($db_database) or die("Unable to select database: " . mysql_error());
             
             
 /* Stop injections */
