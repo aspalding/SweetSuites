208f5daf6a0353243d50c782806bcc7496c1e8dc
Andrew <andrewtspalding@gmail.com>
2013-12-10 14:28:09 -0600

    vulnerability fix.

--- login.php
+++ login.php
@@ -20,9 +20,12 @@ if (isset($_SERVER['PHP_AUTH_USER']) &&
         $row = mysql_fetch_row($result);
         $token = md5("$pw_temp");
 
-        if ($token == $row[3])
+        //Check against password and if user has had more than 5 login attemps.
+        if ($token == $row[3] && $row[4] < 6)
         {
             session_start();
+            
+            session_regenerate_id(true);
 
             $_SESSION['name'] = $un_temp;
             $_SESSION['type'] = $row[1];
@@ -30,7 +33,24 @@ if (isset($_SERVER['PHP_AUTH_USER']) &&
 
             die(header('Location: index.php'));
         }
-        else die("Invalid username/password combination, click back and try again.");
+        else{
+            //If the user has not atttempted to login in an hour, reset attempts to 0.
+            if(time() - strtotime($row[5]) > 60*60){
+                $query = "UPDATE users SET attempts=0 WHERE name='$un_temp'";
+                $result = mysql_query($query);
+            }
+        
+            $query = "UPDATE users SET attempts=attempts + 1 WHERE name='$un_temp'";
+            $result = mysql_query($query);
+            
+            //If the user has had 5 or more logins in the past hour, don't let them login.
+            if($row[4] > 4){
+                die("Max attempts reached, try again later.");
+            }
+            else{
+                die("Invalid username/password combination, click back and try again.");
+            }
+        } 
     }
     else die("Invalid username/password combination, click back and try again.");
 }
